8bcd7dec40a862adba514d70f99ed3c2527ffcf9
Thomas LaGreca <thomlagreca@gmail.com>
2018-09-21 10:22:46 +1000

    Fixes to properties. Added Incomplete animation on open menu.

--- src/popoverMenu.js
+++ src/popoverMenu.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Animated, Easing } from 'react-native';
 import PropTypes from 'prop-types';
 
 class PopoverMenu extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            opened: false,
+            initialHeight: 0,
+            finalHeight: 0
+        }
+
+        this.animatedHeightOpen = new Animated.Value(0);
+        this.animatedHeightClose = new Animated.Value(0);
+
         this.releaseTouchEvent = this.releaseTouchEvent.bind(this);
     }
 
-    static defaultProps = {
-        topOffset: 60,
-        rightOffset: 0,
 
+    componentDidMount() {
+        let finalHeight = React.Children.count(this.props.children) * this.props.rowHeight;
+
+        this.setState({
+            finalHeight
+        }, this.animateMenuOpen());
+  
     }
+
+    componentDidUpdate() {
+        this.animateMenuOpen();
+    }
+
+    componentWillUnmount() {
+        this.animateMenuClose();
+    }
+
+    animateMenuOpen() { 
+        this.animatedHeightOpen.setValue(0);
+
+        Animated.timing(this.animatedHeightOpen, {
+            toValue: this.state.finalHeight,
+            duration: 1000,
+            easing: Easing.ease
+        }).start()
+    }
+
+    animateMenuClose() {
+        this.animatedHeightClose.setValue(this.state.finalHeight);
+
+        Animated.timing(this.animatedHeightClose, {
+            toValue: this.state.initialHeight,
+            duration: 1000,
+            easing: Easing.ease
+        }).start()
+    }
+
+
     // The function responsible for closing the menu. The container
     // of the component is a full screen view with a touch event 
     // listener attached. This acts as the touch-outside-to-close function. 
@@ -22,7 +65,7 @@ class PopoverMenu extends Component {
     }
 
     render() {
-        if (!this.props.showMenu) return <View />;
+        if (!this.props.showMenu) return null;
 
         let rightOffset = this.props.rightOffset || 0;
         let topOffset = this.props.topOffset || 60;
@@ -32,10 +75,10 @@ class PopoverMenu extends Component {
                 style={[styles.container, { marginTop: topOffset, marginRight: rightOffset }]}
                 onStartShouldSetResponder={evt => true}
                 onResponderRelease={this.releaseTouchEvent}>
-                <View
-                    style={styles.menu}>
+                <Animated.View
+                    style={[styles.menu, { height: this.animatedHeightOpen }]}>
                     {this.props.children}
-                </View>
+                </Animated.View>
             </View>
 
         );
@@ -43,6 +86,7 @@ class PopoverMenu extends Component {
 
     }
 };
+
 
 const styles = StyleSheet.create({
     container: {
